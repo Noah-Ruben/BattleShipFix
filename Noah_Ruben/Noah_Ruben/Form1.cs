85ea8efcba0e318b6cf4303bce04eaadfc154b15
Sora1829 <49411352+Sora1829@users.noreply.github.com>
2020-01-10 14:40:00 -0600

    try again

---
+++ Noah_Ruben/Noah_Ruben/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Noah_Ruben
+{
+    public partial class Form1 : Form
+    {
+        Label[][] Board;
+        Image FirstShip = Image.FromFile("boat1.PNG");
+        Image SecondShip = Image.FromFile("boat2.PNG");
+        Image ThirdShip = Image.FromFile("boat3.PNG");
+        Image FourthShip = Image.FromFile("boat4.PNG");
+        Image FifthShip = Image.FromFile("boat5.PNG");
+        Image SixthShip = Image.FromFile("boat6.PNG");
+        int changed = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Label[] row1 = new Label[] { label1, label2, label3, label4, label5, label6, label7, label8, label9, label10 };
+            Label[] row2 = new Label[] { label11, label12, label13, label14, label15, label16, label17, label18, label19, label20 };
+            Label[] row3 = new Label[] { label21, label22, label23, label24, label25, label26, label27, label28, label29, label30 };
+            Label[] row4 = new Label[] { label31, label32, label33, label34, label35, label36, label37, label38, label39, label40 };
+            Label[] row5 = new Label[] { label41, label42, label43, label44, label45, label46, label47, label48, label49, label50 };
+            Label[] row6 = new Label[] { label51, label52, label53, label54, label55, label56, label57, label58, label59, label60 };
+            Label[] row7 = new Label[] { label61, label62, label63, label64, label65, label66, label67, label68, label69, label70 };
+            Label[] row8 = new Label[] { label71, label72, label73, label74, label75, label76, label77, label78, label79, label80 };
+            Label[] row9 = new Label[] { label81, label82, label83, label84, label85, label86, label87, label88, label89, label90 };
+            Label[] row10 = new Label[] { label91, label92, label93, label94, label95, label96, label97, label98, label99, label100 };
+            Board = new Label[][] { row1, row2, row3, row4, row5, row6, row7, row8, row9, row10 };
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            panel1.Hide();
+        }
+
+        private void label116_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(changed <= 12)
+            {
+                Board[Convert.ToInt32(textBox1.Text)][Convert.ToInt32(textBox2.Text)].Image = FirstShip;
+                Board[Convert.ToInt32(textBox4.Text)][Convert.ToInt32(textBox3.Text)].Image = SecondShip;
+                Board[Convert.ToInt32(textBox6.Text)][Convert.ToInt32(textBox5.Text)].Image = ThirdShip;
+                Board[Convert.ToInt32(textBox12.Text)][Convert.ToInt32(textBox11.Text)].Image = FourthShip;
+                Board[Convert.ToInt32(textBox10.Text)][Convert.ToInt32(textBox9.Text)].Image = FifthShip;
+                Board[Convert.ToInt32(textBox8.Text)][Convert.ToInt32(textBox7.Text)].Image = SixthShip;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox1.Text != null)
+            {
+                changed++;
+                this.textBox1.TextChanged -= textBox1_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox2.Text != null)
+            {
+                changed++;
+                this.textBox2.TextChanged -= textBox2_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox4.Text != null)
+            {
+                changed++;
+                this.textBox4.TextChanged -= textBox4_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox3.Text != null)
+            {
+                changed++;
+                this.textBox3.TextChanged -= textBox3_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox6.Text != null)
+            {
+                changed++;
+                this.textBox6.TextChanged -= textBox6_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox5.Text != null)
+            {
+                changed++;
+                this.textBox5.TextChanged -= textBox5_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox12.Text != null)
+            {
+                changed++;
+                this.textBox12.TextChanged -= textBox12_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox11.Text != null)
+            {
+                changed++;
+                this.textBox11.TextChanged -= textBox11_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox10.Text != null)
+            {
+                changed++;
+                this.textBox10.TextChanged -= textBox10_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox9.Text != null)
+            {
+                changed++;
+                this.textBox9.TextChanged -= textBox9_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox8.Text != null)
+            {
+                changed++;
+                this.textBox8.TextChanged -= textBox8_TextChanged;
+                break;
+            }
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            while (textBox7.Text != null)
+            {
+                changed++;
+                this.textBox7.TextChanged -= textBox7_TextChanged;
+                break;
+            }
+        }
+    }
+}
